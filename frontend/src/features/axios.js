76361974163109b3bf7125416b6f70c9b40f9380
Nahuel Cassinari <nahuel_cassinari@hotmail.com>
2022-08-12 00:20:21 +0200

    change env variable name

--- frontend/src/features/axios.js
+++ frontend/src/features/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const API_URI_DOMAIN = process.env.REACT_APP_URI_DOMAIN || 'http://localhost:8000'
+const API = process.env.REACT_APP_API || 'http://localhost:8000'
 
 export default axios.create({
-	baseURL: API_URI_DOMAIN
+	baseURL: API
 })
